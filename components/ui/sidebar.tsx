4bec48da743917cb49d971ae0fc9a017d68a4c76
H <speedparcel.senawang@gmail.com>
2026-08-28 05:07:16 +0000

    initial commit

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -138,7 +138,7 @@ function SidebarProvider({
           } as React.CSSProperties
         }
         className={cn(
-          "group/sidebar-wrapper flex min-h-svh w-full has-data-[variant=inset]:bg-sidebar",
+          "group/sidebar-wrapper relative flex min-h-svh w-full has-data-[variant=inset]:bg-sidebar",
           className
         )}
         {...props}
@@ -307,7 +307,9 @@ function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
     <main
       data-slot="sidebar-inset"
       className={cn(
-        "relative flex w-full flex-1 flex-col bg-background md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2",
+        "relative flex w-full flex-1 flex-col bg-background",
+        "peer-data-[variant=inset]:m-2 peer-data-[variant=inset]:ml-0 peer-data-[variant=inset]:rounded-xl peer-data-[variant=inset]:shadow-sm",
+        "md:peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon))] md:peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]",
         className
       )}
       {...props}
